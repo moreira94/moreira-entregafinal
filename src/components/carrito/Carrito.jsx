818b9fe9de3ded8a00ce70b3ba553c3ec1cda43e
Julian Moreira <moreirajulian.gustavo@gmail.com>
2024-01-19 15:33:21 -0300

    a

--- src/components/carrito/Carrito.jsx
+++ src/components/carrito/Carrito.jsx
@@ -37,7 +37,7 @@ export const Carrito = () => {
 
             </div>
             </div> : <div></div>}
-            
+
             { isNaN(carrito) ? 
                 carrito.map((prod, index) => (
                     <div className="carrito-item" key={prod.id}>
@@ -63,5 +63,6 @@ export const Carrito = () => {
             }
 
         </div>
+        
     )
 }
