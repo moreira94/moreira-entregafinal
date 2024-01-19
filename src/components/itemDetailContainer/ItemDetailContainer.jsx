6114a649f792bf5d3d015e6957f63fff65e8e058
Julian Moreira <moreirajulian.gustavo@gmail.com>
2024-01-18 22:06:24 -0300

    se centralizo el contexto y se rearmo el filtro

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -1,25 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ItemDetail } from '../itemDetail/ItemDetail'
 import { getProductById } from '../../funciones'
 import { useParams } from 'react-router-dom'
 import { doc, getDoc } from "firebase/firestore"
 import { database } from '../../firebase/config'
+import { MoviesContext } from '../../context/MoviesContext'
 
 export const ItemDetailContainer = () => {
 
     const [item, setItem ] = useState(null)
     const id= useParams().id;
+    const movies = useContext(MoviesContext);
 
-    // useEffect(() => {
-
-    //     const docRef = doc(database, "productos", id);
-    //     getDoc(docRef)
-    //     .then((resp) =>{
-    //         setItem(
-    //             {...resp.data(), id : resp.id}
-    //         )
-    //     })
-    // }, [id])
+    useEffect(() => {
+      const itemFiltrado = movies.movies.filter(m => m.id == id)
+      setItem(itemFiltrado[0])
+    }, [id])
 
   return (   
     <div className='itemDetailContainer'>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,5 +3,34 @@ import { createContext } from "react";
 
 export const CartContext = createContext();
 
+const CartProvider = ({children}) => {
+    const [carrito, setCarrito] = useState([])
+    const cantidadEnCarrito = () => {
+      return carrito.reduce((acc,prod) => acc + prod.cantidad, 0)  
+  }
+    const precioTotal = () => {
+      return carrito.reduce((acc, prod) => acc + prod.price * prod.cantidad, 0)
+    }
+  
+    const vaciarCarrito = () => {
+      setCarrito([]);
+    }
+
+    const contextValue = {
+        carrito,
+        setCarrito,
+        cantidadEnCarrito,
+        precioTotal,
+        vaciarCarrito,
+    }
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
 
 
